5143d77823ed96924c96860aeeef6a6cefa346c3
Bensonodhiambo <benodhiambo.bo@gmail.com>
2024-10-04 20:30:17 +0000

    Taskar

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,17 +1,19 @@
 export default function createIteratorObject(report) {
-  const employees = [];
+  const employees = []; // Create an array to hold all employees
 
   // Collect all employees from each department
   for (const department in report.allEmployees) {
-    employees.push(...report.allEmployees[department]); // Spread the employees into the array
+    if (Object.prototype.hasOwnProperty.call(report.allEmployees, department)) {
+      employees.push(...report.allEmployees[department]); // Spread the employees into the array
+    }
   }
 
   // Create a generator function to yield each employee
   function* employeeIterator() {
     for (const employee of employees) {
-      yield employee;
+      yield employee; // Yield each employee
     }
   }
 
-  return employeeIterator(); // Return the generator
+  return employeeIterator(); // Return the generator iterator
 }
