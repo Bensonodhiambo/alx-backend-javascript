cf66b7b41434638b6d39b28d4a299328eb9fe689
Bensonodhiambo <benodhiambo.bo@gmail.com>
2024-10-04 18:58:25 +0000

    Task7Correction

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,5 +1,3 @@
-export default function getBudgetObject(income, gdp, capita) {
-  const budget = { income, gdp, capita };
+const getBudgetObject = (income, gdp, capita) => ({ income, gdp, capita });
 
-  return budget;
-}
+export default getBudgetObject;
