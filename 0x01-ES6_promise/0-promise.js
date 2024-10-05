ed9c95ca1c52ef30755c5e04dca7e4f6809ee1f9
Bensonodhiambo <benodhiambo.bo@gmail.com>
2024-10-05 02:38:00 +0000

    Task0

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,6 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    // You can perform asynchronous operations here, but for now, we'll resolve it directly
+    resolve('Success');
+  });
+}
